258ce97c3a0399820b600210f4ad1c5bbfa7c7fe
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2019-12-11 20:55:43 +0100

    React tutorial components

--- app-react/App.js
+++ app-react/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 class App extends Component {
   render() {
+    var myStyle = {  
+        fontSize: 80,  
+        fontFamily: 'Courier',  
+        color: '#003300'  
+     } 
+     let i = 5;
     return (
       <div>
-        <h1>Hello ReatJS Applications</h1>
-      </div>
+        <h1>JavaTpoint</h1>
+        <h2>Training Institutes</h2>
+        <p style={myStyle}>This website contains the best CS tutorials.</p>
+        <h1>{i == 1 ? 'True!' : 'False!'}</h1>  
+      </div>  
     );
   }
 }
