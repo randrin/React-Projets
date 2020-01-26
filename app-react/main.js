cb8b84645712af5dad3238d7ace830b211fe0288
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-01-26 14:33:06 +0100

    Reat Routers

--- app-react/main.js
+++ app-react/main.js
@@ -4,8 +4,37 @@ import App from "./components/App.js";
 import Forms from "./components/Forms";
 import LoginConditional from "./components/LoginConditional";
 import Reference from "./components/Reference";
+import {Route, Link, Switch, BrowserRouter as Router, NavLink} from 'react-router-dom'
+import NotFound from "./components/NotFound";
 
-ReactDOM.render(<App />, document.getElementById("app"));
-ReactDOM.render(<Forms />, document.getElementById("forms"));
-ReactDOM.render(<LoginConditional />, document.getElementById("login"));
-ReactDOM.render(<Reference />, document.getElementById("ref"));
+
+const routing = (
+    <Router>
+        <div>
+            <h2>React Js Router</h2>
+            <ul>
+                <li>
+                    <NavLink exact activeStyle={{color: 'red'}} to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink exact activeStyle={{color: 'green'}} to="/forms">React Forms</NavLink>
+                </li>
+                <li>
+                    <NavLink exact activeStyle={{color: 'orange'}} to="/login">React Logical Conditional</NavLink>
+                </li>
+                <li>
+                    <NavLink exact activeStyle={{color: 'yellow'}} to="/reference">React References</NavLink>
+                </li>
+            </ul>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/forms" component={Forms} />
+                <Route path="/login" component={LoginConditional} />
+                <Route path="/reference" component={Reference} />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById("app"));
