22369e7180d442b5fafbda2a2fb8c3b2513f0789
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-04-15 23:48:28 +0200

    Visit-cities: Get data Mocks fron json to view

--- visit-cities/src/components/city/city.js
+++ visit-cities/src/components/city/city.js
@@ -3,9 +3,24 @@ import "./city.scss";
 
 export default class City extends Component {
   render() {
+    const { id, city, img, name, info } = this.props.city;
+    const { removeCity } = this.props;
     return (
-      <div>
-        <h3>This is the tour</h3>
+      <div className="row city-container">
+        <div className="col-md-6 img-container">
+          <img className="img-city" src={img} title={name} />
+          <span className="img-city-remove">
+            <i className="fa fa-window-close"></i>
+          </span>
+        </div>
+        <div className="col-md-6">
+          <h2 className="city-city">{city}</h2>
+          <h3 className="city-name">{name}</h3>
+          <div className="city-info">
+            <span>Info</span> <i className="fa fa-caret-square-o-down"></i>
+            <p>{info}</p>
+          </div>
+        </div>
       </div>
     );
   }
--- visit-cities/src/components/cityList/cityList.js
+++ visit-cities/src/components/cityList/cityList.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 import "./cityList.scss";
 import City from "../city/city";
+import { tourData } from "./../../tourData";
 
 export default class CityList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cities: tourData,
+    };
+  }
+
+  removeCity = (id) => {
+    console.log("city id: ", id);
+  };
   render() {
+    const { cities } = this.state;
     return (
-      <div>
+      <section className="container">
         <h2>List Cities visit</h2>
-        <City />
-      </div>
+        {cities.map((city) => (
+          <City key={city.id} city={city} removeCity={this.removeCity} />
+        ))}
+      </section>
     );
   }
 }
